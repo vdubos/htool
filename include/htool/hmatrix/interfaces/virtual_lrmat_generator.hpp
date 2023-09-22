bb1bce7ad52a6a5a91316b866cb76f8ca039e033
Pierre Marchand <pierre.jimthefrench@gmail.com>
2023-09-22 11:48:52 +0200

    add param in/out

--- include/htool/hmatrix/interfaces/virtual_lrmat_generator.hpp
+++ include/htool/hmatrix/interfaces/virtual_lrmat_generator.hpp
@@ -26,13 +26,13 @@ class VirtualLowRankGenerator {
      *
      * The low rank approximation is defined as \f$ \mathbf{U} \mathbf{V}\f$, where \f$\mathbf{U}\f$ has a size \f$ (M \times r)\f$ and \f$\mathbf{V}\f$ has a size \f$ (r \times N)\f$, \f$ r\f$ being the resulting \p rank . The resulting \f$\mathbf{U}\f$ and \f$\mathbf{V}\f$ are stored on output in \p U and \p V.
      *
-     * @param A VirtualGenerator given to generator coefficient of the approximated kernel.
-     * @param t target Cluster that defines the rows of the approximated block
-     * @param s source Cluster that defines the columns of the approximated block
-     * @param epsilon input tolerance
-     * @param rank On input, given tolerance or if \p rank <0, the approximation is expected to continue up to an epsilon tolerance. On output, it is the obtained rank at the end of the low rank approximation.
-     * @param U \f$ \mathbf{U} \f$-part of the low-rank approximation on output
-     * @param V \f$ \mathbf{V} \f$-part of the low-rank approximation on output
+     * @param[in] A VirtualGenerator given to generator coefficient of the approximated kernel.
+     * @param[in] t target Cluster that defines the rows of the approximated block
+     * @param[in] s source Cluster that defines the columns of the approximated block
+     * @param[in] epsilon input tolerance
+     * @param[inout] rank On input, given tolerance or if \p rank <0, the approximation is expected to continue up to an epsilon tolerance. On output, it is the obtained rank at the end of the low rank approximation.
+     * @param[out] U \f$ \mathbf{U} \f$-part of the low-rank approximation on output
+     * @param[out] V \f$ \mathbf{V} \f$-part of the low-rank approximation on output
      */
     virtual void copy_low_rank_approximation(const VirtualGenerator<CoefficientPrecision> &A, const Cluster<CoordinatesPrecision> &t, const Cluster<CoordinatesPrecision> &s, underlying_type<CoefficientPrecision> epsilon, int &rank, Matrix<CoefficientPrecision> &U, Matrix<CoefficientPrecision> &V) const = 0;
 
